9906c4219212206d7b74dbf677682eb252cd5e17
Devansh Mayatra <devanshm667@gmail.com>
2025-03-31 12:33:37 +0530

    added questionnaire model

---
+++ backend/src/models/questionnaire.model.js
@@ -0,0 +1,17 @@
+import { Schema , model } from "mongoose";
+
+const questionnaireSchema = new Schema({
+  title: { type: String, required: true },
+  content:{
+    type:[
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Question',
+      }
+    ]
+  }
+},{
+  timestamps: true
+});
+
+export const Questionnaire = model("Questionnaire",questionnaireSchema);
